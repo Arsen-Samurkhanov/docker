0bf08abf262f472eb3af9fc7b6790fdfe52f2d93
Арсен Самурханов <arsen.samurhanov@maximumtest.ru>
2021-02-03 18:24:36 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           
-          changed
+          on the feature branch
         </p>
         <a
           className="App-link"
